647543c6eae16fcb83ade0f1a51d0f6b5ad0609b
alexandernewton <alex.newton@digital.cabinet-office.gov.uk>
2018-10-28 16:06:02 +0000

    Added save and load capability

--- directory.rb
+++ directory.rb
@@ -20,6 +20,8 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
@@ -35,8 +37,12 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
+  when "4"
+    load_students
   when "9"
-    exit 
+    exit
   else
     puts "I don't know what you meant, try again"
   end
@@ -56,5 +62,25 @@ end
 def print_footer
   puts "Overall, we have #{@students.count} great students"
 end
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
+
 
 interactive_menu
